1441874fcc1022f153865313a21a0a4ebfe1cf86
IvoRibeiro09 <ivomiguelalvesribeiro@gmail.com>
2023-11-08 21:37:03 +0000

    close + pause

--- tp2/fase1/ClienteGUI.py
+++ tp2/fase1/ClienteGUI.py
@@ -14,11 +14,12 @@ class ClienteGUI:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.streansNoRP = None
         self.condition = threading.Condition()
-        self.streamSelected = None
+        self.conditionBool = False
+        self.status = "Playing"
+        self.clientClose = True
         self.parse(file)
         self.clientStart()
 
-    
     def parse(self, file):
         print("Parsing...")
         with open(file, 'r') as f:
@@ -33,14 +34,24 @@ class ClienteGUI:
                         self.adjacentes.append(extrair_neighbour(line))
         
     def clientStart(self):
-        print("Starter...")
-        self.inicialConnection()
-        self.askStreamTransmission()
-        thread = threading.Thread(target=self.streamTransmission())
-        thread.start()
-        self.janela.mainloop()
-        #self.server_socket.close()
-
+        
+            try:
+                print("Starter...")
+                self.inicialConnection()
+                self.askStreamTransmission()
+                thread = threading.Thread(target=self.streamTransmission())
+                thread.start()
+                thread.join()
+                '''
+                self.clinetNewStart()
+                with self.condition:
+                    while not self.conditionBool:
+                        self.condition.wait()
+                self.conditionBool = False
+                '''
+            except Exception as e:
+                print(e)
+            
     def inicialConnection(self):
         #conectar ao servidor 
         socket_address = self.adjacentes[0]
@@ -62,8 +73,9 @@ class ClienteGUI:
     def askStreamTransmission(self):
         self.clienteInterface()
         with self.condition:
-            while self.streamSelected is None:
+            while not self.conditionBool:
                 self.condition.wait()
+        self.conditionBool = False
 
     def clienteInterface(self):
         self.janela = tk.Tk()
@@ -90,7 +102,7 @@ class ClienteGUI:
         mensagem = f"Stream- {video}"
         self.server_socket.sendall((mensagem).encode())
         with self.condition:
-            self.streamSelected = video
+            self.conditionBool = True
             self.condition.notify()
         self.janela.destroy()
     
@@ -104,16 +116,26 @@ class ClienteGUI:
         self.janela = tk.Tk()
         self.janela.title(f"Cliente {self.IP}")
         
-        # Substitui o Canvas por um Label
+        # tela de display de video
         self.label = tk.Label(self.janela, width=640, height=480, bg='white')
-        self.label.pack()
+        self.label.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
 
+
+        # Butao pausa de enviar a stream de video para o cliente				
+        self.botaoPause = tk.Button(self.janela, width=20, padx=3, pady=3)
+        self.botaoPause["text"] = "Pause"
+        self.botaoPause["command"] = self.pauseStream
+        self.botaoPause.grid(row=1, column=0, padx=10, pady=10)
+
+        # Para de streamar o video
         self.botaoClose = tk.Button(self.janela, width=20, padx=3, pady=3)
         self.botaoClose["text"] = "Close"
-        self.botaoClose.pack(padx=2, pady=2)
+        self.botaoClose["command"] =  self.closeStream
+        self.botaoClose.grid(row=1, column=1, padx=10, pady=10)
+        
         # recebe o video em bytes do cliente
         i = 0
-        while True:
+        while not self.server_socket._closed:
             print("Frame: ",i)
             try:
                 # Recebe o tamanho do frame (4 bytes) do servidor
@@ -124,66 +146,80 @@ class ClienteGUI:
                 frame_data = b""
                 while len(frame_data) < frame_size:
                     frame_data += self.server_socket.recv(frame_size - len(frame_data))
+                
+                if self.status == "Playing":
+                    # Converte os dados do frame em uma imagem
+                    img = ImageTk.PhotoImage(data=frame_data)
 
-				# Converte os dados do frame em uma imagem
-                img = ImageTk.PhotoImage(data=frame_data)
-
-				# Atualiza a label na janela Tkinter com a nova imagem
-                self.label.configure(image=img)
-                self.label.image = img
+                    # Atualiza a label na janela Tkinter com a nova imagem
+                    self.label.configure(image=img)
+                    self.label.image = img
+                    #self.janela.update()
                 self.janela.update()
+
             except Exception as e:
                 # Registre a exceção para fins de depuração
                 print(f"Erro ao receber vídeo: {e}")
             i+=1
 
+    def pauseStream(self):
+        if self.status == "Playing":
+            print("Stream paused...")
+            self.status = "Pause"
+            self.botaoPause["text"] = "Resume"
+        elif self.status == "Pause":
+            print("Stream resumed...")
+            self.status = "Playing"
+            self.botaoPause["text"] = "Pause"
+
+    def closeStream(self):
+        print("Closing Stream...")
+        self.server_socket.close()
+        self.janela.destroy()
+    '''
+    def clinetNewStart(self):
+        self.janela = tk.Tk()
+        self.janela.title(f"Cliente {self.IP}")
+        spacing = 10
+        #tela
+        self.label = tk.Label(self.janela, width=60, padx=spacing, pady=spacing)
+        self.label["text"] = "Transmission has ended!!!\nWant to continue watching a new Stream..."
+        self.label.grid(row=0, column=0, padx=spacing, pady=spacing, columnspan=2)
+
+        # Botao streamar e enviar a stream de video para o cliente		
+        self.botaoStart = tk.Button(self.janela, width=30, padx=spacing, pady=spacing)
+        self.botaoStart["text"] = "Yes"
+        self.botaoStart["command"] = self.yesbutton
+        self.botaoStart.grid(row=1, column=0, padx=spacing, pady=spacing)
+	
+        self.botaoStart = tk.Button(self.janela, width=30, padx=spacing, pady=spacing)
+        self.botaoStart["text"] = "No"
+        self.botaoStart["command"] = self.nobutton
+        self.botaoStart.grid(row=1, column=1, padx=spacing, pady=spacing)
+        self.janela.mainloop()
+
+    def yesbutton(self):
+        self.clientClose == True
+        self.conditionBool = True
+        with self.condition:
+            self.condition.notify()
+        self.janela.destroy()
+
+    def nobutton(self):
+        self.clientClose == False
+        self.conditionBool = True
+        with self.condition:
+            self.condition.notify()
+        self.janela.destroy()
+        '''
+        
 
 if __name__ == "__main__":
     try:
-        filename = "config_file.txt"
-        cliente = ClienteGUI(filename)
-        cliente.janela.mainloop()
+        while True:
+            filename = "config_file.txt"
+            cliente = ClienteGUI(filename)
+            cliente.janela.mainloop()
     except Exception as e:
         print(f"Erro: {e}")
         print("[Usage: Cliente.py]\n")
-
-
-
-    '''
-    def clienteInterface2(self): 
-        print("Show interface...")
-        i = 0
-        for strean in self.streansNoRP:
-            #tela com nome
-            self.label = tk.Label(self.janela, width=20, padx=3, pady=3)
-            self.label["text"] = f"{strean}"
-            self.label.grid(row=i, column=0, padx=2, pady=2)
-            # Butao streamar e enviar a stream de video para o cliente		
-            self.botaoStart = tk.Button(self.janela, width=20, padx=3, pady=3)
-            self.botaoStart["text"] = "Play"
-            self.botaoStart["command"] = self.playStream
-            self.botaoStart.grid(row=i, column=1, padx=2, pady=2)
-            # Butao pausa de enviar a stream de video para o cliente				
-            self.botaoPause = tk.Button(self.janela, width=20, padx=3, pady=3)
-            self.botaoPause["text"] = "Pause"
-            self.botaoPause["command"] = self.pauseStream
-            self.botaoPause.grid(row=i, column=2, padx=2, pady=2)    
-            # Para de streamar o video
-            self.botaoClose = tk.Button(self.janela, width=20, padx=3, pady=3)
-            self.botaoClose["text"] = "Close"
-            self.botaoClose["command"] =  self.closeStream
-            self.botaoClose.grid(row=i, column=3, padx=2, pady=2)
-            i+=1
-
-    def playStream(self, video):
-        #pedir stream ao RP
-        print("Pedido de envio do video "+video+ " ao servidor "+ self.socket_address)
-        #receber os dados
-        print("A receber video")
-    
-    def pauseStream(self): 
-        print("Stream Paused...")
-
-    def closeStream(self):
-        print("Stream close")
-    '''
