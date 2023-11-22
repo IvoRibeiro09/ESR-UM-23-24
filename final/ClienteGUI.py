fd0480f101100495e69058f769effb5645cd2240
IvoRibeiro09 <ivomiguelalvesribeiro@gmail.com>
2023-11-22 20:06:56 +0000

    documentation on clienteGui

--- final/ClienteGUI.py
+++ final/ClienteGUI.py
@@ -5,8 +5,10 @@ from PIL import ImageTk
 from Packet import *
 from NodeData import *
 
+'''
+This is the main class for the Client
+'''
 class ClienteGUI:
-
     def __init__(self, node):
         self.node = node
         self.my_address = (NodeData.getIp(self.node), 0)
@@ -20,28 +22,35 @@ class ClienteGUI:
     def clientStart(self):
         try:
             while True:
+                self.status = "Playing"
                 self.inicialConnection()
-                self.streamTransmission()
+                self.recievestreamTransmission()
         except Exception as e:
             print(e)
-            
+    '''
+    Este metodo é usadado para conectar o cliente ao RendezvousPoint onde nessa conexão o cliente
+    é informado de quais streams os servidores podem streamar, e atraves de uma interface gráfica 
+    é possivel selecionar um deles, após selecionado é enviada essa informação ao RP
+    '''
     def inicialConnection(self):
-        #conectar ao servidor 
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as rp_socket:
                 rp_socket.bind(self.my_address)
                 rp_socket.connect(NodeData.getRPAddress(self.node))
         
-                #pedir os videos que ele tem 
+                # Perguntar ao RP quais as Streams que ele tem acesso
                 message = "VideoList"
                 rp_socket.sendall((message).encode())
-                #receber a lista de video do rp
+                
+                # Receber a lista de streams do RP
                 data = rp_socket.recv(1024)
                 mensagem = data.decode()
                 vids = mensagem.split("/")
                 vids.pop()
-                
+                # Selecionar a transmissão
                 self.askStreamTransmission(vids)
+
+                # Enviar a escolha para o RP
                 mensagem = f"Stream- {self.selected}"
                 rp_socket.sendall((mensagem).encode())
                 
@@ -50,15 +59,22 @@ class ClienteGUI:
             print(f"Erro ao conectar ou enviar mensagens: {e}")
         finally:
             rp_socket.close()
-
+    '''
+    Metodos usados para definir a interface gráfica que permite ao cliente selecionar uma opção de stream
+    '''
     def askStreamTransmission(self, streamList):
+        # Chamar a interface gráfica
         self.clienteInterface(streamList)
+
+        # Esperar a escolha por parte do cliente
         with self.condition:
             while not self.conditionBool:
                 self.condition.wait()
         self.conditionBool = False
 
+    # Interface gráfica
     def clienteInterface(self, streamList):
+        # Criar a janela
         self.janela = tk.Tk()
         self.janela.title(f"Cliente {NodeData.getIp(self.node)}")
         self.janela.geometry("+1000+50")
@@ -66,29 +82,38 @@ class ClienteGUI:
         i = 0
         spacing = 10
         for stream in streamList:
-            #tela com nome
+            # mostrar nome da stream
             self.label = tk.Label(self.janela, width=60, padx=spacing, pady=spacing)
             self.label["text"] = f"{stream}"
             self.label.grid(row=i, column=0, padx=spacing, pady=spacing)
 
-            # Botao streamar e enviar a stream de video para o cliente		
+            # Botao para selecionar a stream 		
             self.botaoStart = tk.Button(self.janela, width=30, padx=spacing, pady=spacing)
             self.botaoStart["text"] = "Select"
             self.botaoStart["command"] = lambda s=stream: self.selectStream(s)
             self.botaoStart.grid(row=i, column=1, padx=spacing, pady=spacing)
             i+=1
         self.janela.mainloop()
-
+        
+    # metodo que garante a seleção de uma stream
     def selectStream(self, video):
-        print(f"{video} has been selected...")
+        # Destroir a janela
         self.janela.destroy()
         with self.condition:
+            # Notificar que o cliente ja selecionou uma stream
             self.selected = video
             self.conditionBool = True
             self.condition.notify()
+        print(f"{video} has been selected...")
     
-    def streamTransmission(self):
-        print("Cliente aguarda video...")
+    '''
+    Metodos que proporcionam a receção de uma stream e consequentemente mostrar essa reção ao cliente 
+    atraves de uma interface gráfica
+    De notar: cada frame é dividido em 3 pacotes no seu envio devido à dimensão dos mesmos e por isso 
+    utilizamos metricas de controlo de perda de pacotes na receção dos mesmos
+    '''
+    def recievestreamTransmission(self):
+        # Criar a janela
         self.janela = tk.Tk()
         self.janela.title(f"Cliente {NodeData.getIp(self.node)}")
         self.janela.geometry("+1000+50")
@@ -97,47 +122,43 @@ class ClienteGUI:
         self.label = tk.Label(self.janela, width=640, height=480, bg='white')
         self.label.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
 
-        # Butao pausa de enviar a stream de video para o cliente				
+        # Butao pausa de visualização da stream pelo cliente				
         self.botaoPause = tk.Button(self.janela, width=20, padx=3, pady=3)
         self.botaoPause["text"] = "Pause"
         self.botaoPause["command"] = self.pauseStream
         self.botaoPause.grid(row=1, column=0, padx=10, pady=10)
 
-        # Para de streamar o video
+        # Para fechar a transmissao de video para o cliente
         self.botaoClose = tk.Button(self.janela, width=20, padx=3, pady=3)
         self.botaoClose["text"] = "Close"
         self.botaoClose["command"] =  self.closeStream
         self.botaoClose.grid(row=1, column=1, padx=10, pady=10)
-        # recebe o video em bytes do cliente
-        i = 0
+
         my_address_udp = (NodeData.getIp(self.node), NodeData.getStreamPort(self.node))
         try:
+            # Receber os pacotes udp que transportam a informação dos frames em bytes
             with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as socketForStream:
                 socketForStream.bind(my_address_udp)
-                print(f"{my_address_udp} à espera de conexões de Streams: ")
+
                 while self.status != "Closed":
-                    #parse packet | Recebe o tamanho do frame (4 bytes) do servidor
-                    data, _ = socketForStream.recvfrom(Packet_size)
-                    pacote = Packet("", "", "")
-                    Packet.parsePacket(pacote, data)
-                    print("Frame: ", pacote.frameNumber)
-                    if self.status == "Playing": self.packetQueue.append(pacote)
+                    # A cada iteração recebemos três pacotes que sao serializados e caso o cliente estiver a
+                    # visualizar em tempo reasão, não em estado pausa, adicionados a uma lista de pacotes
+                    for i in range(3):
+                        data, _ = socketForStream.recvfrom(Packet_size)
+                        pacote = Packet("", "", "")
+                        Packet.parsePacket(pacote, data)
+                        if self.status == "Playing": 
+                            self.packetQueue.append(pacote)
+                        #print("Frame: ", pacote.frameNumber)
+                        i+=1
 
-                    data1, _ = socketForStream.recvfrom(Packet_size)
-                    pacote1 = Packet("", "", "")
-                    Packet.parsePacket(pacote1, data1)
-                    print("Frame: ", pacote1.frameNumber)
-                    if self.status == "Playing": self.packetQueue.append(pacote1)
-
-                    data2, _ = socketForStream.recvfrom(Packet_size)
-                    pacote2 = Packet("", "", "")
-                    Packet.parsePacket(pacote2, data2)
-                    print("Frame: ", pacote2.frameNumber)
-                    if self.status == "Playing": self.packetQueue.append(pacote2)
-                    #print("msg no pacote: ",Packet.getFrameData(pacote))
-
+                    # Se o cliente estiver a ver a stream em tempo real, verificamos se os trẽs pacotes que formam 
+                    # um frame foram recebidos e se sim criamos um frame e atualizamos a imagem na interface gráfica
                     if self.status == "Playing":
+                        # verificar se é possivel criar um frame com os pacotes que temos
                         frame = self.verifyFrame()
+
+                        # Se for possivel o frame é criado e a imagem atualizada
                         if frame:
                             # Converte os dados do frame em uma imagem
                             img = ImageTk.PhotoImage(data= frame)
@@ -146,57 +167,68 @@ class ClienteGUI:
                             self.label.configure(image=img)
                             self.label.image = img   
                     self.janela.update()
+
         except Exception as e:
             print(f"Erro ao receber vídeo: {e}")
         finally:
+            # Destroir a janela e fechar a conexão udp
             socketForStream.close()
             self.janela.destroy()
-            self.status = "Playing"
             print("Closing Stream...")
-
+    
+    '''
+    Metodo de verficação e correção na perda de pacotes pela rede
+    '''        
+    def verifyFrame(self):
+        # verificar se os 3 elementos da lista sao do mesmo pacote
+        pack1 = self.packetQueue[0]
+        pack2 = self.packetQueue[1]
+        pack3 = self.packetQueue[2]
+        # se sim juntar e devolver o frame
+        if Packet.getFrameNumber(pack1) == Packet.getFrameNumber(pack2) == Packet.getFrameNumber(pack3):
+            self.packetQueue.pop(0)
+            self.packetQueue.pop(0)
+            self.packetQueue.pop(0)
+            return Packet.getFrame(pack1) + Packet.getFrame(pack2) + Packet.getFrame(pack3)
+        # caso contrario eliminar o primeiro pacote da lista de recebidos
+        else:
+            self.packetQueue.pop(0)
+            # Se a lista tiver mais de 3 pacotes verificar se esses tres podem construir um frame
+            if len(self.packetQueue) >= 3:
+                return self.verifyFrame()
+            # Caso contrario não devolve um frame e a imagem não é atualizada
+            else:            
+                return None
+            
+    '''
+    Metodos usados para garantir a funcionalidade dos butões na interface gráfica
+    '''
     def pauseStream(self):
+        # Parar de mostrar os pacotes que estamos a receber
         if self.status == "Playing":
             print("Stream paused...")
             self.status = "Pause"
             self.botaoPause["text"] = "Resume"
+        # Voltar a mostrar os pacotes que esta a receber
         elif self.status == "Pause":
             print("Stream resumed...")
             self.status = "Playing"
             self.botaoPause["text"] = "Pause"
 
     def closeStream(self):
+        # Fechar a recessão de stream
         try:
             self.status = "Closed"
+            # Avisar o RP que não pretende receber mais pacotes
             self.rp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.rp_socket.bind(self.my_address)
             self.rp_socket.connect(NodeData.getRPAddress(self.node))
-            #pedir os videos que ele tem 
+            
             message = "Connection closed"
             self.rp_socket.sendall((message).encode())
-            # o cliente avisa o rp que fechou a conexao
             
             print('The Client informed the RP that he no longer intends to receive the stream.')
         except Exception as e:
             print(f"Erro ao enviar mensagem de fechar a Stream para o servidor: {e}")
         finally:
             self.rp_socket.close()
-            
-    def verifyFrame(self):
-        # verificar se os 3 elementos da lista sao do mesmo pacote
-        pack1 = self.packetQueue[0]
-        pack2 = self.packetQueue[1]
-        pack3 = self.packetQueue[2]
-        if Packet.getFrameNumber(pack1) == Packet.getFrameNumber(pack2) == Packet.getFrameNumber(pack3):
-            self.packetQueue.pop(0)
-            self.packetQueue.pop(0)
-            self.packetQueue.pop(0)
-            return Packet.getFrame(pack1) + Packet.getFrame(pack2) + Packet.getFrame(pack3)
-        else:
-            # se sim juntar e passar para imagem
-            # caso contrario dar pop no primeiro pacote e se o len da queue for maior que tres verificar outra vez se sao o mesmo pacote
-            print("nao coincide")
-            self.packetQueue.pop(0)
-            if len(self.packetQueue) >= 3:
-                return self.verifyFrame()
-            else:            
-                return None
